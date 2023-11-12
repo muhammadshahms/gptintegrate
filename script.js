079cae0311367915ce8625001c0934ae7218ca73
salahshakeel <99357263+salahshakeel@users.noreply.github.com>
2023-11-12 21:48:32 +0500

    .

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-import { config } from "dotenv"
-config()
+import { config } from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
 
-import { Configuration, OpenAIApi } from "openai"
-import readline from "readline"
+// Load environment variables from .env file
+config();
 
+// Create an instance of the OpenAIApi
 const openAi = new OpenAIApi(
   new Configuration({
     apiKey: process.env.OPEN_AI_API_KEY,
   })
-)
+);
 
+// Create a user interface for reading input
 const userInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
-userInterface.prompt()
-userInterface.on("line", async input => {
-  const response = await openAi.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: input }],
-  })
-  console.log(response.data.choices[0].message.content)
-  userInterface.prompt()
-})
+// Display initial prompt
+userInterface.prompt();
+
+// Event handler for user input
+userInterface.on("line", async (input) => {
+  try {
+    const response = await openAi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: input }],
+    });
+
+    console.log(response.data.choices[0].message.content);
+
+    // Introduce a delay before prompting again
+    setTimeout(() => {
+      userInterface.prompt();
+    }, 1000); // Adjust the delay time (in milliseconds) as needed
+  } catch (error) {
+    console.error("Error:", error.message);
+    userInterface.prompt();
+  }
+});
